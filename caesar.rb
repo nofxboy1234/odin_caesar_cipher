50ab81be7207c6bb344bf16f076e7b2bf8357a23
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-27 19:25:05 +1000

    Refactor

--- caesar.rb
+++ caesar.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
+def wrap_z_to_a; end
+
 def caesar_cipher(text, key)
   new_text = ''
   text.each_char do |char|
     a_ord = 'a'.ord
     z_ord = 'z'.ord
     char_ord = char.downcase.ord
-
     was_downcased = (char != char.downcase)
-
     target_ord = char_ord + key
 
     if char.match(/^[[:alpha:]]$/)
@@ -16,10 +16,10 @@ def caesar_cipher(text, key)
         wrap_offset = target_ord - z_ord
 
         new_char = (a_ord + wrap_offset - 1).chr
-        new_char.upcase! if was_downcased
       else
         new_char = target_ord.chr
       end
+      new_char.upcase! if was_downcased
       new_text += new_char
     else
       new_text += char
@@ -31,6 +31,7 @@ end
 
 encrypted_text = caesar_cipher('What a string!', 5)
 # encrypted_text = caesar_cipher('F', -5)
+# encrypted_text = caesar_cipher('a', -1)
 
 puts "encrypted_text: #{encrypted_text}"
 
